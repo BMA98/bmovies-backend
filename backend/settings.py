3bf0d87a42c44e6ac579d5e054be772fed7a4555
Bastian M <bastian.matamala@ug.uchile.cl>
2020-09-15 00:40:03 -0300

    chore: add allowed hosts

--- backend/settings.py
+++ backend/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'https://bmovie-backend.azurewebsites.net/']
 
 CORS_ORIGIN_ALLOW_ALL = True
 
