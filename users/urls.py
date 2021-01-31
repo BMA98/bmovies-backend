72c73d7325177976eb7a62afb93181070ac45169
Bastian M <bastian.matamala@ug.uchile.cl>
2021-01-30 22:56:56 -0300

    feat: new viewset for requesting top stars

--- users/urls.py
+++ users/urls.py
@@ -4,7 +4,7 @@ from django.urls import path, include
 from rest_framework.routers import SimpleRouter
 
 from users.viewsets import MovieSeenViewSet, MovieRankViewSet, MovieOnlyRankViewSet, UserFavoriteMovieViewSet, \
-    UserFullFavoriteMovieViewSet, MovieDetailedRankViewSet, UsersViewSet
+    UserFullFavoriteMovieViewSet, MovieDetailedRankViewSet, UsersViewSet, UserTopStars
 
 router = SimpleRouter()
 router.register('rest-auth/user/movies/ranks/detail', MovieDetailedRankViewSet)
@@ -12,9 +12,9 @@ router.register('rest-auth/user/movies/ranks', MovieRankViewSet)
 router.register('rest-auth/user/movies/seen', MovieSeenViewSet)
 router.register('rest-auth/user/movies/favorites/full', UserFullFavoriteMovieViewSet)
 router.register('rest-auth/user/movies/favorites', UserFavoriteMovieViewSet)
+router.register('rest-auth/user/stars', UserTopStars)
 router.register('rest-auth/users', UsersViewSet)
 router.register('ranks', MovieOnlyRankViewSet)
-
 urlpatterns = router.urls
 
 urlpatterns += [
