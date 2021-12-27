43979c79328321d769a1ec322332cea32b65bea1
Bastian Matamala <bastian.matamala@ug.uchile.cl>
2021-12-27 01:39:28 -0300

    feat: new endpoint for requesting data from top stars overall

--- users/viewsets/stats.py
+++ users/viewsets/stats.py
@@ -1,4 +1,5 @@
 import datetime
+import random
 
 from django.http import HttpResponse
 from rest_framework import viewsets
@@ -8,21 +9,23 @@ from rest_framework.response import Response
 from rest_framework.decorators import action
 from django.db.models import Count, Sum
 
+from users.serializers import MovieHistorySerializer
+
 
 class DataView(viewsets.ViewSet):
     queryset = MovieHistory.objects.all()
 
     @action(detail=False)
     def report(self, request, **kwargs):
-        print(f'Authentication: {self.authenticate(**kwargs)}')
         if not self.authenticate(**kwargs):
             return HttpResponse(status=401)
         try:
-            year = self.request.query_params["year"]
+            year = self.request.query_params['year']
+            user = self.request.query_params['user_id']
         except KeyError:
             year = datetime.date.today().year
-        if request.user.id:
-            self.queryset = self.queryset.filter(user_id=request.user.id)
+            user = self.request.user.id
+        self.queryset = self.queryset.filter(user_id=user)
         self.queryset = self.queryset.filter(timestamp__year=year)
         # Count by language
         languages = self.queryset.values('movie__language') \
@@ -100,7 +103,10 @@ class DataView(viewsets.ViewSet):
         monthly_viewing = self.queryset.values('timestamp__month') \
             .annotate(count=Count('movie__tmdb_id')) \
             .order_by('timestamp__month')
-        monthly_viewing = [record['count'] for record in list(monthly_viewing)]
+        tmp = [0] * 12
+        for record in list(monthly_viewing):
+            tmp[record['timestamp__month'] - 1] = record['count']
+        monthly_viewing = tmp
         # Sum runtime
         total_runtime = self.queryset.aggregate(Sum('movie__runtime'))
         # Count by genre
@@ -115,6 +121,20 @@ class DataView(viewsets.ViewSet):
         countries = {record['movie__countries__iso']: record['count'] for record in list(countries)}
         # Total movies
         total = MovieHistory.objects.filter(timestamp__year=year).distinct('movie_id').count()
+        history = self.queryset.order_by('timestamp') \
+            .values('id',
+                    'movie__tmdb_id',
+                    'movie__original_title',
+                    'timestamp',
+                    'movie__poster',
+                    'movie__backdrop')
+        history = [{'id': record['id'],
+                    'tmdb_id': record['movie__tmdb_id'],
+                    'original_title': record['movie__original_title'],
+                    'timestamp': record['timestamp'],
+                    'poster': record['movie__poster'],
+                    'backdrop': record['movie__backdrop'],
+                    } for record in history]
         response = {'top_languages': languages,
                     'top_stars': top_stars[:3],
                     'top_directors': top_directors[:3],
@@ -124,9 +144,9 @@ class DataView(viewsets.ViewSet):
                     'monthly_viewing': monthly_viewing,
                     'top_genres': genres,
                     'countries': countries,
-                    'total': total
+                    'total': total,
+                    'history': history,
                     }
-        # Count by country
         return Response(response)
 
     def authenticate(self, **kwargs):
@@ -134,8 +154,69 @@ class DataView(viewsets.ViewSet):
             return True
         else:
             token = self.request.query_params['token']
-            print(self.request.query_params['user_id'])
-            print(self.request.query_params['token'])
             x = URLToken.objects.filter(user=self.request.query_params['user_id'], token=token).count()
-            #print(x)
             return x >= 1
+
+    @action(detail=False)
+    def top_stars(self, request, **kwargs):
+        if not self.authenticate(**kwargs):
+            return HttpResponse(status=401)
+        try:
+            user = self.request.query_params['user_id']
+            year = self.request.query_params['year']
+        except KeyError:
+            user = self.request.user.id
+            year = datetime.date.today().year
+        self.queryset = self.queryset.filter(user_id=user)
+        top_stars = self.queryset \
+            .values('movie__movierole__star_id',
+                    'movie__movierole__star__name',
+                    'movie__movierole__star__profile_path') \
+            .annotate(count=Count('movie__tmdb_id', distinct=True)) \
+            .order_by('-count')
+        print(top_stars)
+        top_stars = [{'tmdb_id': record['movie__movierole__star_id'],
+                      'name': record['movie__movierole__star__name'],
+                      'count': record['count'],
+                      'profile': record['movie__movierole__star__profile_path']
+                      }
+                     for record in list(top_stars)]
+        max = top_stars[0]['count']
+        top_star_ids = []
+        for star in top_stars:
+            if star['count'] == max:
+                top_star_ids.append(star['tmdb_id'])
+            else:
+                break
+        # top_star = tmp[random.randint(0, len(tmp) - 1)]
+        movies = self.queryset.filter(timestamp__year=year, movie__movierole__star_id__in=top_star_ids) \
+            .values('movie__movierole__star_id',
+                    'movie__movierole__star__profile_path',
+                    'movie__movierole__star__name',
+                    'movie__movierole__movie__original_title',
+                    'movie__movierole__movie__tmdb_id',
+                    'movie__movierole__movie__poster',
+                    )
+        movies_dict = {}
+        for movie in movies:
+            if movie['movie__movierole__star_id'] in movies_dict:
+                movies_dict[movie['movie__movierole__star_id']]['movies'] \
+                    .append(
+                    {
+                        'tmdb_id': movie['movie__movierole__movie__tmdb_id'],
+                        'original_title': movie['movie__movierole__movie__original_title'],
+                        'poster': movie['movie__movierole__movie__poster'],
+                    })
+
+            else:
+                movies_dict[movie['movie__movierole__star_id']] = {
+                    'name': movie['movie__movierole__star__name'],
+                    'count': max,
+                    'profile_path': movie['movie__movierole__star__profile_path'],
+                    'movies': [{
+                        'tmdb_id': movie['movie__movierole__movie__tmdb_id'],
+                        'original_title': movie['movie__movierole__movie__original_title'],
+                        'poster': movie['movie__movierole__movie__poster'],
+                    }]
+                }
+        return Response(movies_dict)
