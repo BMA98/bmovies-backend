1f267e87848b1193abd0df9b3024b11a56fd089c
Bastian Matamala <bastian.matamala@ug.uchile.cl>
2022-07-18 02:13:01 -0400

    feat: change filterset field names

--- users/filtersets.py
+++ users/filtersets.py
@@ -1,3 +1,4 @@
+import django_filters
 from django_filters import rest_framework as filters
 
 from users.models import MovieHistory
@@ -5,11 +6,17 @@ from users.models import MovieHistory
 
 class MovieHistoryFilterSet(filters.FilterSet):
 
+    language = filters.CharFilter(field_name='movie__language', lookup_expr='exact')
+    tmdb_id = filters.NumberFilter(field_name='movie_id', lookup_expr='exact')
+
+    class NumberInFilter(filters.BaseInFilter, filters.NumberFilter):
+        pass
+
+    tmdb_id__in = NumberInFilter(field_name='movie_id', lookup_expr='in')
+
     class Meta:
         model = MovieHistory
         fields = {
             'user': ['exact'],
-            'timestamp': ['exact', 'year', 'year__gt', 'year__lt'],
-            'movie_id': ['exact', 'in'],
-            'movie__language': ['exact'],
+            'timestamp': ['exact', 'year', 'year__gt', 'year__lt']
         }
